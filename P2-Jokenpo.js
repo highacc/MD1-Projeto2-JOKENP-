8ed7b17091d7db664ca9c94ea4bef8f5eff1297b
Olavo e Souto <olavo_souto@hotmail.com>
2022-03-18 16:57:35 -0300

    Finalizado

--- P2-Jokenpo.js
+++ P2-Jokenpo.js
@@ -5,126 +5,143 @@ const prompt = require('prompt-sync')();
 // Inicio do jogo
 
 
-let start = prompt("Começar o Jogo? Dugite: sim ou nao: ").toLowerCase()
+let start = prompt("Começar o Jogo? Dugite: sim ou nao: ").toLowerCase();
+
 while (start != "sim" && start != "nao") {
     console.log(`\nComando invalido. Digite apenas sim ou nao.\n`);
     start = prompt("Começar o Jogo? Dugite: sim ou nao: ").toLowerCase();
 }   
 
 if (start == "nao") {
-    console.log()
-    console.log("Fim do Jogo!");
-    console.log()
+    
+    console.log(`\nFim do Jogo!\n`);
+    
     process.exit(1);
+
 }else{
-    console.log('\n\nJOKENPÔ\n\n')
-    console.log(`\n\n`);
+    console.log('\n\nJO..KEN...PÔ!!!\n\n');
+    
 }
+
 // Variaveis contador de Vitorias, Derrotas e Empates
-let vitoria = 0
-let derrota = 0
-let empate = 0
+let vitoria = 0;
+let derrota = 0;
+let empate = 0;
+
+// Bloco geral do jogo, loop para enquanto a variável start == sim
 
 while (start == "sim") {
 
-// Variaveis
+// Variável para saber o número de partidas que o usuário deseja jogar
+let npartidas = +prompt(`\nQuantas partidas gostaria de jogar?   \n`);
 
-
-let npartidas = +prompt(`\nQuantas partidas gostaria de jogar?   \n`)
-
-const jogada = ["Pedra", "Papel", "Tesoura"] 
+const jogada = ["Pedra", "Papel", "Tesoura"] ;
 
 let jogador 
 let computador
 
-for (let i = 0; i <= npartidas; i++) {
+// Bloco de Execução do Jogo
+for (let i = 0; i < npartidas; i++) {
 
     // Jogada do Usuario
 
-    let jokenpo = prompt(`\n\nPedra, Papel ou Tesoura?  \n\n`).toLowerCase()
+    let jokenpo = prompt(`\n\nPedra, Papel ou Tesoura?  \n\n`).toLowerCase();
+
+    //Loop para validação da resposta do usuário, impedindo respostas diferentes das predefinidas.
     while (jokenpo != "pedra" && jokenpo != "papel" && jokenpo != "tesoura") {
 
         console.log(`\nComando invalido. Digite apenas pedra, papel ou tesoura.\n`);
-        jokenpo = prompt("Pedra, Papel ou Tesoura?   ").toLowerCase()
+        jokenpo = prompt("Pedra, Papel ou Tesoura?   ").toLowerCase();
 
     }   
-
+    // Condicional que Informa a opção escolhida pelo Usuário e designa a string para a variavel jogador.
     if (jokenpo == "pedra") {
-        jogador = "pedra"
-        console.log(`Você escolheu ${jogada[0]}`)            
+        jogador = "pedra";
+        console.log(`Você escolheu ${jogada[0]}`);           
     }else if (jokenpo == "papel") {
-        jogador = "papel"
-        console.log(`Você escolheu ${jogada[1]}`)         
+        jogador = "papel";
+        console.log(`Você escolheu ${jogada[1]}`) ;        
     } else {
-        jogador = "tesoura"
-        console.log(`Você escolheu ${jogada[2]}`)         
+        jogador = "tesoura";
+        console.log(`Você escolheu ${jogada[2]}`) ;        
     }
 
     // Jogada do computador        
     let numeroRandom = Math.floor(Math.random() * 3);
+
+    // Condicional que Informa a opção sorteada pelo Computador e designa a string para a variavel computador.
     if (numeroRandom == 0) {
-        computador = "pedra"
-        console.log(`Seu adversário escolheu ${jogada[0]}`)            
+        computador = "pedra";
+        console.log(`Seu adversário escolheu ${jogada[0]}`);         
     }else if (numeroRandom == 1) {
-        computador = "papel"
-        console.log(`Seu adversário escolheu ${jogada[1]}`)         
+        computador = "papel";
+        console.log(`Seu adversário escolheu ${jogada[1]}`);         
     } else {
-        computador = "tesoura"
-        console.log(`Seu adversário escolheu ${jogada[2]}`)         
+        computador = "tesoura";
+        console.log(`Seu adversário escolheu ${jogada[2]}`);         
     }
 
-  // Resultado da Partida 
+  // Resultado da Partida, adiciona a potuação do resultado para vitoria, derrota ou empate e Lê para o usuario o resultado. 
   if ( (jogador=="pedra" && computador=="tesoura") || (jogador=="papel" && computador=="pedra") || (jogador=="tesoura" && computador=="papel")) {
     vitoria++
-    console.log()
-    console.log(`Você Venceu!!!`)
-    console.log()
+    
+    console.log(`\nVocê Venceu!!!\n`);
+    
   } else if (jogador==computador) {
     empate++
-    console.log()
-    console.log(`Empate!!!`)
-    console.log()
+    
+    console.log(`\nEmpate!!!\n`);
+   
       
-  } 
-  else {
+  } else {
     derrota++
-    console.log()  
-    console.log(`Você Perdeu!!!`)
-    console.log()
+    
+    console.log(`\nVocê Perdeu!!!\n`);
+    
   }
 
      
 }
+// Resultado da Rodada, informa os resultados cumulativos(caso continue a jogar) das rodadas.
+console.log(`Numero de Vitórias: ${vitoria}`);
+console.log(`Numero de Empates: ${empate}`);
+console.log(`Numero de Derrotas: ${derrota}\n\n`);
 
-console.log(`Numero de Vitórias: ${vitoria}`)
-console.log(`Numero de Empates: ${empate}`)
-console.log(`Numero de Derrotas: ${derrota}\n\n`)
-
-  
+// Condicional que informa ao Jogador o resultado final da rodada.  
 if (vitoria>derrota) {
-    console.log(`\nSua pontuação foi ${vitoria}, Você Venceu!\n\n`)
-}else if (vitoria=derrota){
-    console.log(`\nSua pontuação foi ${vitoria}, Igual a potuação do sue adversário ${derrota}, Você Empatou!\n\n`)
+
+    console.log(`\nSua pontuação foi ${vitoria}, Você Venceu!!!\n\n`);
+
+}else if (vitoria==derrota){
+
+    console.log(`\nSua pontuação foi ${vitoria}, Igual a pontuação do seu adversário ${derrota}, Você Empatou!!!\n\n`);
+
 } else {
-    console.log(`\nSua pontuação foi ${vitoria}, Você Perdeu!\n\n`)
+
+    console.log(`\nSua pontuação foi ${vitoria}, Menor que a pontuação do seu adversário ${derrota}, Você Perdeu!!!\n\n`);
+
 }
 
-
- start = prompt("Gostaria de jogar novamente? Digite sim ou nao:   ")
+// Variável para saber se o Jogador gostaria de continuar o jogo.
+ start = prompt("Gostaria de jogar novamente? Digite sim ou nao:   ");
 
 while (start != "sim" && start != "nao") {
+
     console.log(`\nComando invalido. Digite apenas sim ou nao.\n`);
     start = prompt("Gostaria de jogar novamente?  ");
+    
 }   
 
 if (start == "nao") {
-    console.log()
-    console.log("Fim do Jogo!");
-    console.log()
+    
+    console.log(`\nFim do Jogo!!!\n`);
+    
     process.exit(1);
+
 }else{
-    console.log('\n\nJOKENPÔ\n\n')
-    console.log(``);
+
+    console.log('\n\nJOKENPÔ\n\n');
+    
 }
 
 }
